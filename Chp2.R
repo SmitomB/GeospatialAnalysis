1838b5e144f27942a87b49a248af74ae742d5364
SmitomB <sborah@ncsu.edu>
2022-01-08 11:02:50 -0500

    Added few more basic codes from the book

--- Chp2.R
+++ Chp2.R
@@ -8,3 +8,25 @@ class(cars)
 # typeof of cars
 typeof(cars)
 
+# names of column in cars
+names(cars)
+
+# summary of the data set
+summary(cars)
+
+# Details of an object
+str(cars)
+
+# If we create a factor for the speed variable by cutting it at its 
+# quartiles, we can contrast how the plot method displays the relationship 
+# between two numerical variables and a numerical variable and factor
+cars$qspeed <- cut(cars$speed, breaks = quantile(cars$speed), include.lowest = T)
+# check if factored
+is.factor(cars$qspeed)
+
+# plots
+plot(dist ~ speed, data = cars)
+plot(dist ~ qspeed, data = cars)
+
+# dummy variables
+lm(dist ~ qspeed, data = cars)
